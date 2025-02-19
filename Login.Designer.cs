e3bcc4f6b8bad865f338d9c5369b2f0bf19a74d4
JOSEPHACS <calderonsaltos1234@gmail.com>
2025-02-18 23:50:08 -0800

    v3.4

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -215,7 +215,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Login";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Login_Load);
             this.panel1.ResumeLayout(false);
