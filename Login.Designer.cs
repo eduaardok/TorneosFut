28d8560873dc1d72a19f4cb22b6d2bf330c66570
JOSEPHACS <calderonsaltos1234@gmail.com>
2025-02-20 20:29:56 -0800

    login60/100

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -50,7 +50,7 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(16)))), ((int)(((byte)(80)))));
+            this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Controls.Add(this.pictureBox2);
             this.panel1.Controls.Add(this.X);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
