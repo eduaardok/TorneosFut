03a9440676a95e6222702417ece2e1a909dafbf0
eduaardok <eduardoreinoso27@gmail.com>
2025-03-22 13:26:19 -0500

    bien

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 namespace TorneosFut
@@ -13,6 +15,9 @@ namespace TorneosFut
         [STAThread]
         static void Main()
         {
+            CultureInfo culture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
